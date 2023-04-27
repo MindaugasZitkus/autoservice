da164347b9654d6ee79a2458addbf57b626406b9
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-04-27 14:56:26 +0300

    35 Django Paskaita + Padaryti nukreipimai is puslapio "/" (defaulto) i "autoservice/"  redirect

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Service, Order, Vehicle
+
 
 # Create your views here.
+def index(request):
+    context = {
+        "service_count": Service.objects.count(),
+        "orders_done": Order.objects.filter(status__exact="i").count(),
+        "vehicles": Vehicle.objects.count(),
+
+    }
+    return render(request, 'index.html', context=context)
