dea9ba469ea0137b4284ceacd74f403e31bd3460
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-08 14:14:04 +0300

    40  Paskaita, Django  + Į administratoriaus puslapį pridėti šiuos du laukus.

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -19,8 +19,9 @@ class ServiceAdmin(admin.ModelAdmin):
     list_display = ['name','price']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['vehicle','date']
+    list_display = ['vehicle','date','client','deadline']
     inlines = [OrderLineInLine]
+    list_editable = ['client', 'deadline']
 
 
 
