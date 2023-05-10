55c0091ccebc3e960f13214075fbe6eee3afe77e
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-10 14:36:20 +0300

    43  Paskaita, Django Vartotojo Profilis  + +Padaryti, kad profilio puslapyje būtų galima redaguoti vartotojo informaciją, pakeisti nuotrauką.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,6 +42,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'autoservice',
     'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -169,3 +171,5 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
